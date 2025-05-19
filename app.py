86990878e1defd28e1f469a8dbfa92b4e1ecc802
vishnudas545 <93525378+vishnudas545@users.noreply.github.com>
2025-05-19 20:22:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html.tmpl',app_version="Version1",my_message=message)
+
+if __name__ == '__main__':
+    app_port = os.getenv("APP_PORT",8000)
+    message = os.getenv("MESSAGE",None)
+
+    app.run(host="0.0.0.0",port=app_port)
